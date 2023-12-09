cf26e6302256d5f90be335a728cbc64d268242c7
Freakyy Noob <freakyynoob@gmail.com>
2023-12-08 21:41:40 -0500

    first commit

---
+++ src/components/achievements/constants.ts
@@ -0,0 +1,26 @@
+export const ACHIEVEMENTS = [
+  {
+    title: "Best Coder of the month",
+    logo: "https://fastly-production.24c.in/cars24/seo/static/1_20230830_1693395013.png",
+    duration: "January 2023",
+    content: "",
+  },
+  {
+    title: "Best Coder of the month",
+    logo: "https://fastly-production.24c.in/cars24/seo/static/1_20230830_1693395013.png",
+    duration: "September 2022",
+    content: "",
+  },
+  {
+    title: "Rookie of the Quarter - Tech",
+    logo: "https://fastly-production.24c.in/cars24/seo/static/1_20230830_1693395013.png",
+    duration: "2021-2022",
+    content: "",
+  },
+  {
+    title: "Employee of the month",
+    logo: "https://www.neogrowth.in/wp-content/uploads/2021/05/cropped-logo_new-1.png.webp",
+    duration: "March 2021",
+    content: "",
+  },
+];
